5bf9a94d49c3716384f1366fbce66857d46fae13
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2022-12-02 13:53:35 +0000

    Cleaning repo

--- aws_image_uploader.sh
+++ aws_image_uploader.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-echo "Account ID:"
-read account_id
-echo "Image Name:"
-read image_name
+
+account_id = $1
+image_name = $2
+tag = "${3:-latest}"
 
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin $account_id.dkr.ecr.eu-west-2.amazonaws.com
 aws ecr create-repository --repository-name $image_name --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
-docker tag  $image_name:latest $account_id.dkr.ecr.eu-west-2.amazonaws.com/$image_name:latest
-docker push $account_id.dkr.ecr.eu-west-2.amazonaws.com/$image_name:latest
+docker tag  $image_name:$tag $account_id.dkr.ecr.eu-west-2.amazonaws.com/$image_name:$tag
+docker push $account_id.dkr.ecr.eu-west-2.amazonaws.com/$image_name:$tag
