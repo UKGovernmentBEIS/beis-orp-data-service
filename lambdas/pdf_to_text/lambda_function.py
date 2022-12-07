ee2e489ff670322bcc4310404fd3cdfe762cf108
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2022-12-07 18:01:38 +0000

    Added writing to doc database step

--- lambdas/pdf_to_text/lambda_function.py
+++ lambdas/pdf_to_text/lambda_function.py
@@ -9,6 +9,8 @@ from pdfminer.layout import LAParams, LTChar, LTFigure, LTTextBox, LTTextLine
 import re
 from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 import io
+from uuid import uuid4
+import pymongo
 
 MIN_CHARS = 6
 MAX_WORDS = 20
@@ -228,7 +230,9 @@ def handler(event, context):
     bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
     object_key = event["Records"][0]["s3"]["object"]["key"]
     object_size = event["Records"][0]["s3"]["object"]["size"]
+    object_id = uuid4()
     print(f"New document in {bucket_name}: {object_key}, with size: {object_size}")
+    print(f"Assigning document the unique ID: {object_id}")
 
     s3_client = boto3.client('s3')
     doc_stream = s3_client.get_object(
@@ -245,9 +249,29 @@ def handler(event, context):
     s3_client.put_object(
         Body=text,
         Bucket=bucket_name,
-        Key=f'processed/{title}.txt'
+        Key=f'processed/{object_id}.txt'
     )
 
+    # Create a MongoDB client, open a connection to Amazon DocumentDB as a
+    # replica set and specify the read preference as secondary preferred
+    client = pymongo.MongoClient(
+        """mongodb://ddbadmin:Test123456789@beis-orp-dev-beis-orp.cluster-cau6o2mf7iuc.eu-west-2.docdb.amazonaws.com:27017/
+    ?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false""")
+    db = client.documents
+
+    # Specify the collection to be used
+    col = db.testing
+
+    # Insert a single document
+    col.insert_one({
+        "_id": object_id,
+        "title": title
+    })
+
+    # Close the connection
+    client.close()
+
+    # Insert code to put document into DocumentDB with key = object_id and title = title
     return {
         'statusCode': 200
     }
