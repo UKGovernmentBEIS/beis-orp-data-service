f5080bcaffa7458feeb5bffb95f39dd95a5679e2
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2022-12-12 15:32:08 +0000

    Updated pdf_to_text to pull UUID from S3 metadata

--- lambdas/pdf_to_text/lambda_function.py
+++ lambdas/pdf_to_text/lambda_function.py
@@ -9,9 +9,9 @@ from pdfminer.layout import LAParams, LTChar, LTFigure, LTTextBox, LTTextLine
 import re
 from openpyxl.cell.cell import ILLEGAL_CHARACTERS_RE
 import io
-import os
-import pymongo
-from bson import ObjectId
+# import os
+# import pymongo
+# from bson import ObjectId
 
 MIN_CHARS = 6
 MAX_WORDS = 20
@@ -231,48 +231,57 @@ def handler(event, context):
     bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
     object_key = event["Records"][0]["s3"]["object"]["key"]
     object_size = event["Records"][0]["s3"]["object"]["size"]
-    object_id = ObjectId()
-
-    print(f"New document in {bucket_name}: {object_key}, with size: {object_size}")
-    print(f"Assigning document the unique ID: {object_id}")
-
-    os.system('wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem')
 
     s3_client = boto3.client('s3')
+
     doc_stream = s3_client.get_object(
         Bucket=bucket_name,
         Key=object_key
     )['Body']
 
+    metadata = s3_client.head_object(
+        Bucket=bucket_name,
+        Key=object_key
+    )['Metadata']
+
     doc_bytes = doc_stream.read()
     doc_bytes_io = io.BytesIO(doc_bytes)
 
-    # doc_bytes = doc_stream.read()
     title, text = extract_title_and_text_from_all_pages(doc_bytes_io)
+
+    uuid = metadata['uuid']
+
+    print(f"New document in {bucket_name}: {object_key}, with size: {object_size}")
+    print(f"Title of document: {title}")
+    print(f"Document text: {text}")
+    print(f"UUID obtained is: {uuid}")
 
     # Create a MongoDB client, open a connection to Amazon DocumentDB as a
     # replica set and specify the read preference as secondary preferred
-    db_client = pymongo.MongoClient(
-        ("mongodb://ddbadmin:Test123456789@beis-orp-dev-beis-orp.cluster-cau6o2mf7iuc."
-         "eu-west-2.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&"
-         "replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"))
-    db = db_client.documents
-    col = db.testing
 
-    col.insert_one(
-        {
-            "_id": object_id,
-            "title": title
-        }
-    )
+    # os.system('wget https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem')
 
-    db_client.close()
+    # db_client = pymongo.MongoClient(
+    #     ("mongodb://ddbadmin:Test123456789@beis-orp-dev-beis-orp.cluster-cau6o2mf7iuc."
+    #      "eu-west-2.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&"
+    #      "replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"))
+    # db = db_client.documents
+    # col = db.testing
 
-    s3_client.put_object(
-        Body=text,
-        Bucket=bucket_name,
-        Key=f'processed/{object_id}.txt'
-    )
+    # col.insert_one(
+    #     {
+    #         "title": title,
+    #         "uuid": uuid
+    #     }
+    # )
+
+    # db_client.close()
+
+    # s3_client.put_object(
+    #     Body=text,
+    #     Bucket=bucket_name,
+    #     Key=f'processed/{object_id}.txt'
+    # )
 
     return {
         'statusCode': 200
