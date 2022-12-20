521db1305a72058760ba45b2df55cfc86a0ae386
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2022-12-20 14:32:51 +0000

    Created TypeDB ingestion lambda

---
+++ lambdas/typedb_ingestion/typedb_ingestion.py
@@ -0,0 +1,49 @@
+import boto3
+import pymongo
+import json
+
+
+SOURCE_DATABASE = ("mongodb://ddbadmin:Test123456789@beis-orp-dev-beis-orp.cluster-cau6o2mf7iuc."
+                   "eu-west-2.docdb.amazonaws.com:27017/?directConnection=true")
+DESTINATION_QUEUE_URL = "https://sqs.eu-west-2.amazonaws.com/455762151948/update-typedb"
+
+
+def handler(event, context):
+    print(event["Records"][0])
+    document_uuid = event["Records"][0]["s3"]["lambda"]["document_uuid"]
+
+    # Create a MongoDB client and open a connection to Amazon DocumentDB
+    print("Connecting to DocumentDB")
+    db_client = pymongo.MongoClient(
+        SOURCE_DATABASE,
+        tls=True,
+        tlsCAFile="./rds-combined-ca-bundle.pem"
+    )
+    print("Connected to DocumentDB")
+
+    db = db_client.bre_orp
+    collection = db.documents
+
+    query = {
+        "uuid": document_uuid
+    }
+
+    # Find document matching the UUID
+    document = collection.find_one(query)
+
+    # Test query and print the result to the screen
+    print(f"Document found: {document}")
+    db_client.close()
+
+    sqs = boto3.client("sqs")
+    print("Sending document to SQS")
+    response = sqs.send_message(
+        QueueUrl=DESTINATION_QUEUE_URL,
+        MessageBody=json.dumps(document)
+    )
+
+    print(response)
+
+    return {
+        "statusCode": 200
+    }
