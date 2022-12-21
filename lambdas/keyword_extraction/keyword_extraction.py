f87a92dc98c9d0e10f12f800105c1e1511af009c
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2022-12-21 16:40:24 +0000

    Updates to keyword-extraction Lambda

--- lambdas/keyword_extraction/keyword_extraction.py
+++ lambdas/keyword_extraction/keyword_extraction.py
@@ -1,11 +1,9 @@
 import pymongo
 import boto3
-# from keybert import KeyBERT
 import wordninja
 from sklearn.feature_extraction.text import CountVectorizer
 import os
 import re
-# from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 import nltk
 from smart_open import open as smart_open
 import torch
@@ -15,21 +13,20 @@ from bs4 import BeautifulSoup
 from nltk.stem import WordNetLemmatizer
 import zipfile
 
-print('Initiating WordNetLemmatizer')
+
+SOURCE_BUCKET = "beis-orp-dev-datalake"
+
 wnl = WordNetLemmatizer()
 print('Initiated WordNetLemmatizer')
 
-
-# # Define new directory to tmp directory
+# Define new directory to tmp directory
 save_path = os.path.join('/tmp', 'nltk_data')
 os.makedirs(save_path, exist_ok=True)
-# nltk.download('stopwords', download_dir = save_path)
-# nltk.download('punkt', download_dir=save_path)
-nltk.download('wordnet', download_dir = save_path)
-nltk.download('omw-1.4', download_dir = save_path)
-# nltk.download('punkt')
 
-# unzip wordnet
+nltk.download('wordnet', download_dir=save_path)
+nltk.download('omw-1.4', download_dir=save_path)
+
+# Unzip wordnet
 with zipfile.ZipFile(os.path.join(save_path, "corpora", "wordnet.zip"), 'r') as zip_ref:
     zip_ref.extractall(os.path.join(save_path, "corpora"))
 with zipfile.ZipFile(os.path.join(save_path, "corpora", "omw-1.4.zip"), 'r') as zip_ref:
@@ -37,30 +34,11 @@ with zipfile.ZipFile(os.path.join(save_path, "corpora", "omw-1.4.zip"), 'r') as 
 
 print(os.listdir(save_path))
 
-# # Stopwords
-# stopwords = open(os.path.join(save_path, "corpora/stopwords/english"), "r").read()
-# stopwords = stopwords.split("\n")
-# english_stop_words = [w for w in ENGLISH_STOP_WORDS]
-# stopwords.extend(["use", "uses", "used", "www", "gov",
-#                    "uk", "guidance", "pubns", "page"])
-# stopwords.extend(english_stop_words)
-
-# nltk.data.path.append("keyword_extraction/nltk_data")
-
 stopwords = open("./stopwords.txt", "r")
 stopwords = stopwords.read()
 stopwords = [i for i in stopwords.split("\n")]
 stopwords.extend(["use", "uses", "used", "www", "gov",
                   "uk", "guidance", "pubns", "page"])
-
-# Import KeyBERT
-# model_path = os.path.join('/tmp', 'modeldir')
-# os.makedirs(model_path)
-# with open(model_path + "/keybert.py", "rb") as f:
-#     f.write(kw_model = KeyBERT())
-#     f.close()
-
-# from tempfile import NamedTemporaryFile
 
 
 def download_model(
@@ -71,13 +49,12 @@ def download_model(
     save_path = os.path.join('/tmp', 'modeldir')
     os.makedirs(save_path, exist_ok=True)
     s3_resource.Bucket(bucket).download_file(key, os.path.join(save_path, key))
-    # Load the model in
+
+    # Load the model
     with smart_open(os.path.join(save_path, key), 'rb') as f:
         buffer = io.BytesIO(f.read())
         model = torch.load(buffer)
         return model
-
-# Define tokenization function
 
 
 def pre_process_tokenization_function(
@@ -99,9 +76,9 @@ def pre_process_tokenization_function(
     for w in word_tokens:
         if w not in remove_stop_words:
             filtered_sentence.append(w)
-    # # Remove any small characters remaining
+    # Remove any small characters remaining
     filtered_sentence = [word for word in filtered_sentence if len(word) > 1]
-    # # Lemmatise text
+    # Lemmatise text
     lemmatised_sentence = [wnl.lemmatize(word) for word in filtered_sentence]
     return lemmatised_sentence
 
@@ -111,24 +88,6 @@ def pre_process_tokenization_function(
 vectorizer_model = CountVectorizer(
     stop_words="english",
     tokenizer=pre_process_tokenization_function)
-
-# Define download text from bucket function
-
-
-def download_sample_text(
-        s3_client,
-        bucket='beis-orp-dev-datalake',
-        prefix='processed_keyword_extraction'):
-
-    response = s3_client.list_objects_v2(
-        Bucket=bucket, Prefix=prefix, StartAfter=prefix,)
-    s3_files = response["Contents"]
-    latest = max(s3_files, key=lambda x: x['LastModified'])
-    file_content = s3_client.get_object(Bucket=bucket, Key=latest["Key"])[
-        "Body"].read().decode('utf-8')
-    return file_content
-
-# Extract keywords
 
 
 def extract_keywords(text, kw_model):
@@ -142,34 +101,23 @@ def extract_keywords(text, kw_model):
 
 def handler(event, context):
 
-    bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
+    print(f"Event received: {event}")
+    # document_uid = event["document_uid"]
+    # object_key = event["object_key"]
     object_key = event["Records"][0]["s3"]["object"]["key"]
-    object_size = event["Records"][0]["s3"]["object"]["size"]
-
-    print(f"New document in {bucket_name}: {object_key}, with size: {object_size}")
 
     s3_client = boto3.client('s3')
     s3_resource = boto3.resource('s3')
 
-    # doc_stream = s3_client.get_object(
-    #     Bucket=bucket_name,
-    #     Key=object_key
-    # )['Body']
+    # Download text from Data Lake
+    doc_stream = s3_client.get_object(
+        Bucket=SOURCE_BUCKET,
+        Key=object_key
+    )['Body'].read().decode("utf-8")
 
-    # metadata = s3_client.head_object(
-    #     Bucket=bucket_name,
-    #     Key=object_key
-    # )['Metadata']
-
-    # uuid = metadata['uuid']
-
-    # download text
-    input_data = download_sample_text(
-        s3_client=s3_client)
-
-    # classify text
+    # Classify Text
     kw_model = download_model(s3_resource)
-    keywords = extract_keywords(input_data, kw_model)
+    keywords = extract_keywords(doc_stream, kw_model)
 
     print(f"Keywords predicted are: {keywords}")
 
@@ -182,9 +130,7 @@ def handler(event, context):
         tls=True,
         tlsCAFile="./rds-combined-ca-bundle.pem"
     )
-
     print(db_client.list_database_names())
-
     print("Connected to DocumentDB")
 
     # Define document database
@@ -199,5 +145,7 @@ def handler(event, context):
     print("Keywords updated in documentDB")
 
     return {
-        'statusCode': 200
+        "statusCode": 200,
+        "document_uid": test_uuid,
+        "object_key": object_key
     }
