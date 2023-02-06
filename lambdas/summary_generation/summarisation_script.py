d8f67672670005c18ad25aa703cb32485b9f89ac
belin-mxt <114476573+belin-mxt@users.noreply.github.com>
2023-02-06 10:11:53 +0000

    lambdarising summarisation scripts

--- lambdas/summary_generation/summarisation_script.py
+++ lambdas/summary_generation/summarisation_script.py
@@ -1,12 +1,39 @@
+import os
 import torch
-from model_builder import ExtSummarizer
+import boto3
+import pymongo
+from http import HTTPStatus
 from ext_sum import summarize
+from model_builder import ExtSummarizer
+from aws_lambda_powertools.logging.logger import Logger
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+
+logger = Logger()
+
+DOCUMENT_DATABASE = os.environ['DOCUMENT_DATABASE']
+SOURCE_BUCKET = os.environ['SOURCE_BUCKET']
+
+
+def download_text(s3_client, document_uid, Bucket=SOURCE_BUCKET):
+    '''Downloads the raw text from S3 ready for keyword extraction'''
+
+    document = s3_client.get_object(
+        Bucket=SOURCE_BUCKET,
+        Key=f'processed/{document_uid}.txt'
+    )['Body'].read().decode('utf-8')
+
+    logger.info('Downloaded text')
+
+    return document
+
 
 def load_model(model_type):
     # Load model checkpoint
     checkpoint = torch.load(f"checkpoints/{model_type}_ext.pt", map_location="cpu")
     model = ExtSummarizer(checkpoint=checkpoint, bert_type=model_type, device="cpu")
     return model
+
 
 def smart_shortener(text):
     if len(text.split(" ")) < 600:
@@ -16,6 +43,7 @@ def smart_shortener(text):
         shortened_complete = shortened + text.replace(shortened, "").split(".")[0]
         return shortened_complete
 
+
 def clean_summary(summary):
     summary_list = summary.strip().split(" ")
     enum_summary = enumerate(summary_list)
@@ -24,8 +52,49 @@ def clean_summary(summary):
             summary_list.insert(idx + 1, ".")
     return " ".join(summary_list)
 
-def lambda():
+
+def mongo_connect_and_update(document_uid,
+                             summary,
+                             database=DOCUMENT_DATABASE,
+                             tlsCAFile='./rds-combined-ca-bundle.pem'):
+    '''Connects to the DocumentDB, finds the document matching our UUID and adds the summary to it'''
+
+    db_client = pymongo.MongoClient(
+        database,
+        tls=True,
+        tlsCAFile=tlsCAFile
+    )
+    logger.info(db_client.list_database_names())
+    db = db_client.bre_orp
+    collection = db.documents
+
+    # Insert document to DB
+    logger.info({'document': collection.find_one(
+        {'document_uid': document_uid})})
+    collection.find_one_and_update({'document_uid': document_uid}, {
+                                   '$set': {'summary': summary}})
+    db_client.close()
+
+    logger.info('Sent to DocumentDB')
+
+    return {'statusCode': HTTPStatus.OK}
+
+
+def handler(event, context: LambdaContext):
+    logger.set_correlation_id(context.aws_request_id)
+    document_uid = event['document_uid']
+    logger.append_keys(document_uid=document_uid)
+
+    s3_client = boto3.client('s3')
+    document = download_text(s3_client, document_uid)
+
+    logger.info("Loading model")
     model = load_model("mobilebert")
-    shortend_text = smart_shortener(text):
+    shortend_text = smart_shortener(document)
     summary = summarize(smart_shortener(shortend_text), model, max_length=4)
     cleaned_summary = clean_summary(summary)
+
+    response = mongo_connect_and_update(document, cleaned_summary)
+    response['document_uid'] = document_uid
+
+    return response
