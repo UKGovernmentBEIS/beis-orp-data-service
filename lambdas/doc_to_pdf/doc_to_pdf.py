f28b7bdf8966ed0ab56b72d863cd32cff1b95e01
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2023-02-06 17:01:49 +0000

    EOD commit, doc-to-pdf doing my head in

--- lambdas/doc_to_pdf/doc_to_pdf.py
+++ lambdas/doc_to_pdf/doc_to_pdf.py
@@ -1,6 +1,8 @@
-import io
+import brotli
+import tarfile
+from io import BytesIO
 import os
-# import re
+import subprocess
 import boto3
 # from http import HTTPStatus
 from aws_lambda_powertools.logging.logger import Logger
@@ -11,20 +13,37 @@ logger = Logger()
 
 DESTINATION_BUCKET = os.environ['DESTINATION_BUCKET']
 
+libre_office_install_dir = '/tmp/instdir'
 
-def download_text(s3_client, object_key, source_bucket):
+
+def load_libre_office():
+    os.makedirs(libre_office_install_dir, exist_ok=True)
+
+    buffer = BytesIO()
+    with open('/opt/lo.tar.br', 'rb') as brotli_file:
+        d = brotli.Decompressor()
+        while True:
+            chunk = brotli_file.read(1024)
+            buffer.write(d.process(chunk))
+            if len(chunk) < 1024:
+                break
+        buffer.seek(0)
+
+    with tarfile.open(fileobj=buffer) as tar:
+        tar.extractall('/tmp')
+    return f'{libre_office_install_dir}/program/soffice.bin'
+
+
+def download_doc(s3_client, object_key, source_bucket, file_path):
     '''Downloads the PDF from S3 ready for conversion and metadata extraction'''
 
-    document = s3_client.get_object(
+    s3_client.download_file(
         Bucket=source_bucket,
-        Key=object_key
-    )['Body'].read()
+        Key=object_key,
+        Filename=file_path
+    )
 
-    doc_bytes_io = io.BytesIO(document)
-
-    logger.info('Downloaded text')
-
-    return doc_bytes_io
+    return os.listdir('/tmp/')
 
 
 def get_s3_metadata(s3_client, object_key, source_bucket):
@@ -38,15 +57,36 @@ def get_s3_metadata(s3_client, object_key, source_bucket):
     return metadata
 
 
-def write_text(s3_client, text, object_key, metadata,
+def convert_word_to_pdf(soffice_path, word_file_path, output_dir):
+    # conv_cmd = (f"{soffice_path} --headless --norestore --invisible --nodefault --nofirststartwizard --nolockcheck"
+    #             f" --nologo --convert-to pdf:writer_pdf_Export --outdir {output_dir} {word_file_path}")
+    # response = subprocess.run(
+    #     conv_cmd.split(),
+    #     stdout=subprocess.PIPE,
+    #     stderr=subprocess.PIPE)
+    # logger.info(response)
+    # if response.returncode != 0:
+    #     response = subprocess.run(
+    #         conv_cmd.split(),
+    #         stdout=subprocess.PIPE,
+    #         stderr=subprocess.PIPE)
+    #     if response.returncode != 0:
+    #         return False
+    response = subprocess.call([soffice_path, '--headless', '--convert-to',
+                                'pdf', '--outdir', output_dir, word_file_path])
+    logger.info(response)
+    return response
+
+
+def upload_pdf(s3_client, object_key, metadata,
                destination_bucket=DESTINATION_BUCKET):
     '''Write the extracted text to a .txt file in the staging bucket'''
 
-    response = s3_client.put_object(
-        Body=text,
+    response = s3_client.upload_file(
+        Filename=f'/tmp/{object_key}',
         Bucket=destination_bucket,
-        Key=f'trigger-pipeline/{object_key}.pdf',
-        Metadata=metadata
+        Key=f'docs/{object_key}.pdf',
+        # Metadata=metadata
     )
     logger.info('Saved converted document back to S3')
 
@@ -62,20 +102,39 @@ def handler(event, context: LambdaContext):
     logger.info(
         f'New document in {source_bucket}: {object_key}')
 
-    # Download document and associated metadata
-    # Convert to PDF
-    # Write PDF and metadata back to bucket
+    soffice_path = load_libre_office()
+    logger.info(soffice_path)
+
     s3_client = boto3.client('s3')
 
+    response = download_doc(
+        s3_client=s3_client,
+        object_key=object_key,
+        source_bucket=source_bucket,
+        file_path=f'/tmp/{object_key}')
+    logger.info(response)
+
     doc_s3_metadata = get_s3_metadata(
-        s3_client=s3_client, object_key=object_key, source_bucket=source_bucket)
-    document_uid = doc_s3_metadata['uuid']
-    logger.append_keys(document_uid=document_uid)
+        s3_client=s3_client,
+        object_key=object_key,
+        source_bucket=source_bucket)
+    logger.info(doc_s3_metadata)
+    # document_uid = doc_s3_metadata['uuid']
+    # logger.append_keys(document_uid=document_uid)
 
-    s3_response = write_text(s3_client=s3_client, object_key=object_key,
-                             document_uid=document_uid)
+    pdf_document = convert_word_to_pdf(
+        soffice_path=soffice_path,
+        word_file_path=f'/tmp/{object_key}',
+        output_dir='/tmp')
+    logger.info(pdf_document)
+    logger.info(os.listdir('/tmp/'))
 
-    handler_response = {**s3_response}
-    handler_response['document_uid'] = document_uid
+    upload_pdf(
+        s3_client=s3_client,
+        object_key=object_key,
+        metadata=doc_s3_metadata)
 
-    return handler_response
+    # handler_response = {**s3_response}
+    # handler_response['document_uid'] = document_uid
+
+    return None
