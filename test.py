9c47c5b83365486eb118634170512c1d6384830d
Ben Jeffrey <ben.jeffrey@mdrx.tech>
2023-02-08 17:26:47 +0000

    Removing unneeded files

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-from keybert import KeyBERT
-
-
-# Define keybert model
-model_path = os.path.join('/tmp', 'modeldir')
-os.makedirs(model_path)
-with open(model_path + "/kw_model", "r") as kw_model:
-    kw_model = KeyBERT()
