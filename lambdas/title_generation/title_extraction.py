c6e1015ac1cc18c79f4c405d24125b7e99975dc5
belin-mxt <114476573+belin-mxt@users.noreply.github.com>
2023-02-10 11:37:05 +0000

    updates to title extraction

--- lambdas/title_generation/title_extraction.py
+++ lambdas/title_generation/title_extraction.py
@@ -7,6 +7,7 @@ import pymongo
 import numpy as np   
 import pandas as pd 
 from logging import Logger
+from nltk.corpus import stopwords
 from preprocess.preprocess_functions import preprocess
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM 
 from postprocess.postprocess_functions import postprocess_title
@@ -20,12 +21,6 @@ SOURCE_BUCKET = os.environ['SOURCE_BUCKET']
 MODEL_BUCKET = os.environ['MODEL_BUCKET']
 NLTK_DATA_PATH = os.environ['NLTK_DATA']
 MODEL_PATH = os.environ['MODEL_PATH']
-
-
-# Import pre-trained title extraction model
-
-tokenizer = AutoTokenizer.from_pretrained("fabiochiu/t5-small-medium-title-generation")
-model = AutoModelForSeq2SeqLM.from_pretrained("fabiochiu/t5-small-medium-title-generation")
 
 my_pattern = re.compile(r'\s+')
 
@@ -47,21 +42,25 @@ def use_automatic_title_extraction(title):
     """
     params: title: metadata title extracted from the PDF
     """
+    # Remove stopwords
+    title = " ".join([word for word in title.strip().split(" ") if word not in stopwords.words("english")])
     # Remove punctuation
     title = re.sub(r"[^\w\s]", "", title).strip()
     # Remove Microsoft Word from titles
     title = re.sub("Microsoft Word", "", title)
     # Remove excess white space
     title = re.sub(my_pattern, " ", title)
-    # Heuristic: if the number of tokens in the title is less than 4
+    # Heuristic: if the number of tokens in the title is less than 4 or greater than 35
     # Then use automatic title extraction
-    if 35 < len(title.split(" ")) < 4:
+    title_length = len(title.split(" "))
+    if (title_length > 35) or (title_length < 4):
         return True
     else:
         return False
 
 # Define predictor function
-def title_predictor(text):
+def title_predictor(text, tokenizer, model):
+
     # Preprocess the text
     text = preprocess(text)
     inputs = ["summarize: " + text]
@@ -69,6 +68,7 @@ def title_predictor(text):
     output = model.generate(**inputs, num_beams=10, do_sample=False, min_length=10, max_new_tokens=25)
     decoded_output = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
     predicted_title = nltk.sent_tokenize(decoded_output.strip())[0]
+
     # Postprocess the text
     processed_title = postprocess_title(predicted_title)
     return processed_title
@@ -86,13 +86,20 @@ def download_text(s3_client, document_uid, bucket=SOURCE_BUCKET):
     return document
 
 def handler(event, context: LambdaContext):
+
     s3_client = boto3.client('s3')
     document_uid = event['document_uid']
     metadata_title = extract_title()
+
     if use_automatic_title_extraction(title):
+
+        # Import pre-trained title extraction model
+        tokenizer = AutoTokenizer.from_pretrained("fabiochiu/t5-small-medium-title-generation")
+        model = AutoModelForSeq2SeqLM.from_pretrained("fabiochiu/t5-small-medium-title-generation")
         text = download_text(s3_client, document_uid)
-        title = title_predictor(text)
+        title = title_predictor(text, tokenizer, model)
         return title
+
     else:
         title = re.sub("Microsoft Word - ", "", metadata_title)
         title = re.sub(my_pattern, " ", title)
