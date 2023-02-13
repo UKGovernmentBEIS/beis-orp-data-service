b598f8d47226b2a68aef8574e6eb822ac8dc9949
belin-mxt <114476573+belin-mxt@users.noreply.github.com>
2023-02-13 12:21:38 +0000

    progress on html_extraction lambda

--- lambdas/html_extraction/html_extraction.py
+++ lambdas/html_extraction/html_extraction.py
@@ -1,7 +1,19 @@
-from bs4 import BeautifulSoup
-import requests
-from htmldate import find_date
 import re
+import os
+import boto3
+import pymongo
+import requests
+from http import HTTPStatus
+from bs4 import BeautifulSoup
+from htmldate import find_date
+from aws_lambda_powertools.logging.logger import Logger
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+
+logger = Logger()
+
+DOCUMENT_DATABASE = os.environ['DOCUMENT_DATABASE']
+SOURCE_BUCKET = os.environ['SOURCE_BUCKET']
 
 
 URL = "https://www.hse.gov.uk/simple-health-safety/gettinghelp/index.htm"
@@ -11,6 +23,18 @@ HSE_URL = "https://www.hse.gov.uk/simple-health-safety/gettinghelp/index.htm"
 EA_URL = "https://www.gov.uk/check-flooding"
 
 req = requests.get(HSE_URL)
+
+def download_html(s3_client, document_uid, bucket=SOURCE_BUCKET):
+    '''Downloads the raw text from S3 ready for keyword extraction'''
+
+    html = s3_client.get_object(
+        Bucket=bucket,
+        Key=f'raw/{document_uid}.html'
+    )['Body'].read()
+
+    logger.info('Downloaded text')
+
+    return html
 
 
 def get_title_text(req):
@@ -43,6 +67,60 @@ def get_publication_modification_date(URL):
         modification_date = "Inferred " + str(find_date(URL))
 
     return publication_date, modification_date
+
+
+def mongo_connect_and_push(document_uid,
+                           title,
+                           date_published,
+                           database=DOCUMENT_DATABASE,
+                           tlsCAFile='./rds-combined-ca-bundle.pem'):
+    '''Connects to the DocumentDB, finds the document matching our UUID and adds the title, text and publishing date to it'''
+
+    db_client = pymongo.MongoClient(
+        database,
+        tls=True,
+        tlsCAFile=tlsCAFile
+    )
+    logger.info(db_client.list_database_names())
+    db = db_client.bre_orp
+    collection = db.documents
+
+    # Insert document to DB
+    logger.info({'document': collection.find_one(
+        {'document_uid': document_uid})})
+    collection.find_one_and_update({'document_uid': document_uid}, {
+                                   '$set': {'title': title},
+                                   '$set': {'date_published': date_published}})
+    db_client.close()
+
+    logger.info('Sent to DocumentDB')
+
+    return {'statusCode': HTTPStatus.OK}
+
+
+def handler(event, context: LambdaContext):
+
+    s3_client = boto3.client('s3')
+
+    logger.set_correlation_id(context.aws_request_id)
+
+    # Get document id
+    document_uid = event['document_uid']
+
+    # Download raw pdf and extracted text
+    html = download_html(s3_client, document_uid)
+
+    # Get metadata title
+    title, text = get_title_text(html)
+
+    date_published = get_publication_modification_date(URL)
+
+    logger.info(f"Document title is: {title}")
+
+    response = mongo_connect_and_push(document_uid, title, text, date_published)
+    response['document_uid'] = document_uid
+
+    return response
 
 
 # Get title and text
