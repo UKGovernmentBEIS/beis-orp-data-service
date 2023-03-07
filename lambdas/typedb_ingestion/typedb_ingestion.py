80eac30d57b0c4c32e6d78614e2f7ac0dd756ccf
Ben Jeffrey <ben.jeffrey@mdrx.tech>
2023-03-07 17:17:32 +0000

    Large bugfix to typedb_ingestion lambda

--- lambdas/typedb_ingestion/typedb_ingestion.py
+++ lambdas/typedb_ingestion/typedb_ingestion.py
@@ -1,6 +1,7 @@
 import json
 import os
 import boto3
+from operator import itemgetter
 from aws_lambda_powertools.logging.logger import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
@@ -10,25 +11,37 @@ logger = Logger()
 DESTINATION_SQS_URL = os.environ['DESTINATION_SQS_URL']
 
 
-def merge_dicts(*args):
-    merged_dict = {}
+def merge_dicts(dict_list):
+    '''Merge all (changed) inputs into one dictionary'''
 
-    # Merge all dictionaries into the new dictionary
-    for dictionary in args:
-        for key, value in dictionary.items():
-            if value is None:
-                continue
-            if key not in merged_dict:
-                merged_dict[key] = value
-            elif merged_dict[key] == value:
-                continue
-            elif isinstance(merged_dict[key], list):
-                if value not in merged_dict[key]:
-                    merged_dict[key].append(value)
-            else:
-                merged_dict[key] = [merged_dict[key], value]
+    merged_dict = {}
+    for dictionary in dict_list:
+        if dictionary['lambda'] == 'date_generation':
+            merged_dict['data']['dates']['date_published'] = dictionary['document']['data']['dates']['date_published']
+        elif dictionary['lambda'] == 'title_generation':
+            merged_dict['title'] = dictionary['document']['title']
+        elif dictionary['lambda'] == 'keyword_extraction':
+            merged_dict['subject_keywords'] = dictionary['document']['subject_keywords']
+        elif dictionary['lambda'] == 'summarisation':
+            merged_dict['summary'] = dictionary['document']['summary']
+        else:
+            raise UserWarning('Unexpected lambda input received')
 
     return merged_dict
+
+
+def assert_same_base_values(keys, dict_list):
+    '''
+    Raises an assertion error if the inputs received from the parallel stage 
+    don't have the same base values
+    '''
+    # Get the values of the specified keys in each dictionary
+    values = set(itemgetter(*keys)(d['document']) for d in dict_list)
+    # Check if all values are the same
+    assert len(
+        values) == 1, f'The base values of the inputs received are not the same'
+
+    return itemgetter(*keys)(dict_list[0])
 
 
 def sqs_connect_and_send(document, queue=DESTINATION_SQS_URL):
@@ -47,16 +60,17 @@ def sqs_connect_and_send(document, queue=DESTINATION_SQS_URL):
 def handler(event, context: LambdaContext):
     logger.set_correlation_id(context.aws_request_id)
 
-    # Ensuring the outputs of the parallel stage are the same
-    # assert all(map(lambda x: x == event[0], event)
-    #            ), f'Outputs of parallel stage are not the same: {event}'
+    # Ensuring the base outputs of the parallel stage are the same
+    base_keys = ['document_uid', 'regulator_id', 'user_id',
+                 'uri', 'document_type', 'status']
+    base_document = assert_same_base_values(keys=base_keys, dict_list=event)
 
     # Each previous lambda has added a new key to the extracted metadata
     # so we need to merge the metadata docs
-    lambda_responses = [response['document'] for response in event]
-    document = merge_dicts(*lambda_responses)
-
+    inferred_document = merge_dicts(dict_list=event)
+    document = {**base_document, **inferred_document}
     logger.info({'document': document})
+
     response = sqs_connect_and_send(document=document)
     logger.info({'sqs_response': response})
 
