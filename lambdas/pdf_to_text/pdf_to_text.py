e61ee6c816145ca8f814ed3f6d9e3a7b63b54e13
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2023-03-23 17:13:21 +0000

    Added date uploaded field to pdf to text lambda

--- lambdas/pdf_to_text/pdf_to_text.py
+++ lambdas/pdf_to_text/pdf_to_text.py
@@ -172,7 +172,6 @@ def handler(event, context: LambdaContext):
 
     # Finding the time the object was uploaded
     date_uploaded = event['time']
-    date_uploaded_iso = pd.to_datetime(date_uploaded).isoformat()
 
     s3_client = boto3.client('s3')
     doc_bytes_io = download_text(
@@ -217,7 +216,7 @@ def handler(event, context: LambdaContext):
             'dates':
             {
                 'date_published': date_published,
-                'date_uploaded': date_uploaded_iso
+                'date_uploaded': date_uploaded
             }
         },
         'document_type': document_type,
