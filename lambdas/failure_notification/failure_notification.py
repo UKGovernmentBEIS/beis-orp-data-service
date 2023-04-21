fcc2bc28b68cbf26872b65c4f214dbd5598f572e
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2023-04-21 11:23:36 +0100

    Added email send logic into the failure notification

--- lambdas/failure_notification/failure_notification.py
+++ lambdas/failure_notification/failure_notification.py
@@ -9,6 +9,7 @@ logger = Logger()
 
 COGNITO_USER_POOL = os.environ['COGNITO_USER_POOL']
 SENDER_EMAIL_ADDRESS = os.environ['SENDER_EMAIL_ADDRESS']
+ENVIRONMENT = os.environ['ENVIRONMENT']
 
 
 def get_email_address(user_pool_id, user_sub):
@@ -25,7 +26,7 @@ def get_email_address(user_pool_id, user_sub):
         return None
 
 
-def send_email_structure(sender_email, recipient_email, subject, body):
+def send_email(sender_email, recipient_email, subject, body):
     ses_client = boto3.client('ses')
     try:
         response = ses_client.send_email(
@@ -54,24 +55,6 @@ def send_email_structure(sender_email, recipient_email, subject, body):
         logger.error(f'Email failed to send. Error message: {e}')
 
 
-def send_email(COGNITO_USER_POOL, SENDER_EMAIL_ADDRESS,
-               user_id, url):
-
-    email_address = get_email_address(user_pool_id=COGNITO_USER_POOL, user_sub=user_id)
-    logger.info(f'Pulled email from Cognito: {email_address}')
-
-    if email_address:
-
-        send_email_structure(
-            sender_email=SENDER_EMAIL_ADDRESS,
-            recipient_email=email_address,
-            subject='ORP Upload Rejected',
-            body=f'''We have been unable to process (URL: {url}).
-                Please check the URL is not a redirecting URL.
-                This is a system generated email, please do not reply.'''
-        )
-
-
 @logger.inject_lambda_context(log_event=True)
 def handler(event, context: LambdaContext):
     logger.set_correlation_id(context.aws_request_id)
@@ -79,19 +62,56 @@ def handler(event, context: LambdaContext):
     if event['detail']['object']['key'] == 'HTML':
         document = json.loads(event['body']['body'])
         failed_doc = document['uri']
-        user = document['user_id']
+        document_uid = document.get('document_uid', document.get('uuid'))
+        uploader_id = document['user_id']
     else:
         failed_doc = event['document']['object_key']
-        user = failed_doc['user_id']
+        uploader_id = failed_doc['user_id']
 
-    error = event.get('error')
+    error = event.get('error').get('Error')
+    cause = event.get('error').get('Cause')
 
-    logger.info(f'user: {user}')
-    logger.info(f'failed_doc_key: {failed_doc}')
-    logger.info(f'Error: {error}')
+    uploader_email = get_email_address(
+        user_pool_id=COGNITO_USER_POOL,
+        user_sub=uploader_id)
+
+    logger.error(f'''The ORP failed to ingest a document.
+                 Uploader: {uploader_email}
+                 Document S3 key or URL: {failed_doc}
+                 Error: {error}
+                 Cause: {cause}''')
+
+    if document_uid:
+
+        body = f'''Your document (UUID: {document_uid}) has not been uploaded to the ORP.
+                    It can be viewed in the ORP at
+                    https://app.{ENVIRONMENT}.open-regulation.beis.gov.uk/document/view/{document_uid}?ingested=true
+                    However it will not be searchable as uploading the document caused a {error}.
+                    If you know the cause of this error, please fix it and re-upload the document.
+                    If not, reach out to {SENDER_EMAIL_ADDRESS} and they will look into it further.
+                    Thank you for using the ORP.
+                    This is a system generated email, please do not reply.'''
+
+    else:
+
+        body = f'''Your document (UUID: {document_uid}) has not been uploaded to the ORP.
+                    Uploading the document caused an error: {error}.
+                    If you the know cause of this error, please fix it and re-upload the document.
+                    If not, reach out to {SENDER_EMAIL_ADDRESS} and they will look into it further.
+                    Thank you for using the ORP.
+                    This is a system generated email, please do not reply.'''
+
+    response = send_email(
+        sender_email=SENDER_EMAIL_ADDRESS,
+        recipient_email=uploader_email,
+        subject='ORP Upload Failure',
+        body=body
+    )
 
     return {
-        'user': user,
-        'failed_doc': failed_doc,
-        'error': error
+        'Uploader': uploader_email,
+        'Failed Doc': failed_doc,
+        'Error': error,
+        'Cause': cause,
+        'Email Send HTTP Response': response
     }
