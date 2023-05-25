8618fc6455907455fdd57dea1e0b64b534b038ab
imaneh <hafmane@gmail.com>
2023-05-25 10:16:57 +0100

    doc-del: returning metadata of doc

--- lambdas/delete_document/delete_document.py
+++ lambdas/delete_document/delete_document.py
@@ -1,6 +1,5 @@
 import os
 import json
-from datetime import datetime
 from typedb.client import TransactionType, SessionType, TypeDB
 from aws_lambda_powertools.logging.logger import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
@@ -9,18 +8,35 @@ from aws_lambda_powertools.utilities.typing import LambdaContext
 logger = Logger()
 
 
+def getUniqueResult(results):
+    res = [(i.get_type().get_label().name(), i.get_value())
+           for a in results for i in a.concepts() if i.is_attribute()]
+    return res
+
+def match_query(query, session, group=True):
+    with session.transaction(TransactionType.READ) as transaction:
+        print("Query:\n %s" % query)
+        iterator = transaction.query().match_group(
+            query) if group else transaction.query().match(query)
+        results = [ans for ans in iterator]
+        return results
+    
 def match_delete(session, query):
     with session.transaction(TransactionType.WRITE) as transaction:
         logger.info(f'Query:\n {query}')
         transaction.query().delete(query)
         transaction.commit()
 
+def get_query(uid, regulator_id, session):
+    query = f'match $x isa entity, has document_uid "{uid}",'\
+            f'has regulator_id "{regulator_id}",' \
+            'has document_format $df, has URI $uri;'
+    
+    ans = match_query(query=query, session=session, group=False)
+    results = dict(getUniqueResult(ans))
+    return results
 
-def query_function(event, session):
-
-    uid = event['uuid']
-    regulator_id = event['regulator_id']
-
+def delete_query(uid, regulator_id, session):
     query = f'match $x isa entity, has document_uid "{uid}",'\
             f'has regulator_id "{regulator_id}";' \
             'delete $x isa entity;'
@@ -52,7 +68,6 @@ def handler(event, context: LambdaContext):
     payload = json.loads(event['body'])
 
     logger.info(f'Received event with a payload: {payload}')
-    # payload['time'] = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 
     TYPEDB_IP = validate_env_variable('TYPEDB_SERVER_IP')
     TYPEDB_PORT = validate_env_variable('TYPEDB_SERVER_PORT')
@@ -60,4 +75,12 @@ def handler(event, context: LambdaContext):
 
     client = TypeDB.core_client(TYPEDB_IP + ':' + TYPEDB_PORT)
     session = client.session(TYPEDB_DATABASE_NAME, SessionType.DATA)
-    return query_function(session=session, event=payload)
+
+    uid = event['uuid']
+    regulator_id = event['regulator_id']
+
+    attrs = get_query(uid, regulator_id, session)
+    logger.info(f'Metadata of document about to be deleted: {attrs}')
+    qstatus = delete_query(session=session, event=payload)
+    qstatus['metadata'] = attrs
+    return qstatus
