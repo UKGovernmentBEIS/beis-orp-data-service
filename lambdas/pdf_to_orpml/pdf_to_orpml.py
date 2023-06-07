e88db272c291b58af762925dd2fbddb38db29860
Ben.Jeffrey <ben.jeffrey@mdrx.tech>
2023-06-07 10:19:00 +0100

    Fleshed out the pdf_to_orpml lambda and renamed the function and dir

--- lambdas/pdf_to_orpml/pdf_to_orpml.py
+++ lambdas/pdf_to_orpml/pdf_to_orpml.py
@@ -25,11 +25,8 @@ class CustomHTMLFormatter(HTMLFormatter):
 
 
 def remove_excess_punctuation(text) -> str:
-    '''
-    param: text: Str document text
-        Returns text without excess punctuation
-    '''
-    # Clean punctuation spacing
+    '''Removes excess punctuation'''
+
     text = text.replace(' .', '')
     for punc in string.punctuation:
         text = text.replace(punc + punc, '')
@@ -38,6 +35,7 @@ def remove_excess_punctuation(text) -> str:
 
 def clean_text(text):
     '''Clean the text by removing illegal characters and excess whitespace'''
+
     pattern = re.compile(r'\s+')
 
     text = str(text).replace('\n', ' ')
@@ -58,7 +56,7 @@ def clean_text(text):
     text = text.strip()
     text = text.replace('\t', ' ')
     text = text.replace('_x000c_', '')
-    text = text.encode('ascii', 'ignore').decode("utf-8")
+    text = text.encode('ascii', 'ignore').decode('utf-8')
     text = re.sub('\\s+', ' ', text)
     text = re.sub('<.*?>', '', text)
     text = re.sub('\\.{4,}', '.', text)
@@ -89,31 +87,36 @@ def get_s3_metadata(s3_client, object_key, source_bucket):
         Key=object_key
     )['Metadata']
 
+    logger.info('Downloaded metadata from S3 object')
+
     return metadata
 
 
-def extract_pdf_metadata(doc_bytes_io: BytesIO) -> dict:
+def extract_pdf_metadata(doc_bytes_io: BytesIO) -> list(dict):
     with pdfplumber.open(doc_bytes_io) as pdf:
         metadata = pdf.metadata
 
     if metadata.get('ModDate'):
-        date = datetime.strptime(metadata.get('ModDate')[2:-7], "%Y%m%d%H%M%S")
+        date = datetime.strptime(metadata.get('ModDate')[2:-7], '%Y%m%d%H%M%S')
+        date_formatted = datetime.strftime(date, '%Y-%m-%d')
     elif metadata.get('CreationDate'):
-        date = datetime.strptime(metadata.get('CreationDate')[2:-7], "%Y%m%d%H%M%S")
-    date_formatted = datetime.strftime(date, "%Y-%m-%d")
+        date = datetime.strptime(metadata.get('CreationDate')[2:-7], '%Y%m%d%H%M%S')
+        date_formatted = datetime.strftime(date, '%Y-%m-%d')
 
-    pdf_metadata = {
-        "DC.title": metadata.get('Title'),
-        "DC.subject": metadata.get('Subject'),
-        "DC.date": date_formatted,
-        "DC.publisher": metadata.get('Author'),
-    }
+    pdf_meta_tags = [
+        {'name': 'DC.title', 'content': metadata.get('Title')},
+        {'name': 'DC.subject', 'content': metadata.get('Subject')},
+        {'name': 'DC.date', 'content': date_formatted},
+        {'name': 'DC.publisher', 'content': metadata.get('Author')}
+    ]
 
-    return pdf_metadata
+    logger.info('Extracted metadata from PDF')
+
+    return pdf_meta_tags
 
 
-def extract_pdf_text(doc_bytes_io: BytesIO) -> dict:
-    pages = dict()
+def extract_pdf_text(doc_bytes_io: BytesIO) -> list:
+    pages = list()
     with pdfplumber.open(doc_bytes_io) as pdf:
         for i, page in enumerate(pdf.pages):
             # Extract text content from the page
@@ -121,13 +124,63 @@ def extract_pdf_text(doc_bytes_io: BytesIO) -> dict:
 
             # Remove excess punctuation from the text
             page_content = clean_text(page_content)
-            pages[f'page_{i+1}'] = page_content
+            pages.append(page_content)
+
+    logger.info('Extracted text from PDF')
 
     return pages
 
 
-def process_orpml(pages: dict, pdf_metadata: dict, s3_metadata: dict) -> str:
-    return 'hello'
+def process_orpml(pages: dict, pdf_meta_tags: dict, s3_metadata: dict) -> str:
+
+    orpml = BeautifulSoup(
+        '<!DOCTYPE orpml><orpml><head></head><body></body></orpml>',
+        'html.parser'
+    )
+
+    # Finding the time the object was uploaded
+    date_uploaded = datetime.now()
+    date_uploaded_formatted = date_uploaded.strftime('%Y-%m-%dT%H:%M:%S')
+
+    # Turning the S3 metadata into HTML meta tags
+    regulatory_topics = json.loads(s3_metadata.get('topics'))
+    regulatory_topics_formatted = ', '.join(regulatory_topics)
+
+    s3_meta_tags = [
+        {'name': 'DC.identifier', 'content': s3_metadata['uuid']},
+        {'name': 'DC.regulatorId', 'content': s3_metadata['regulator_id']},
+        {'name': 'DC.userId', 'content': s3_metadata['user_id']},
+        {'name': 'DC.type', 'content': s3_metadata['document_type']},
+        {'name': 'DC.status', 'content': s3_metadata['status']},
+        {'name': 'DC.regulatoryTopic', 'content': regulatory_topics_formatted},
+        {'name': 'DC.dateSubmitted', 'content': date_uploaded_formatted},
+        {'name': 'DC.uri', 'content': s3_metadata['uri']},
+    ]
+
+    meta_tags = pdf_meta_tags + s3_meta_tags
+
+    # Attaching the meta tags to the ORPML header
+    head = orpml.head
+    for meta_tag in meta_tags:
+        new_meta = orpml.new_tag("meta", attrs=meta_tag)
+        head.append(new_meta)
+
+    logger.info('Finished attaching metadata to ORPML header')
+
+    body_tag = orpml.find('body')
+    # Iterate over the pages and append them to the <body> tag
+    for page in pages:
+        # Create a new HTML tag for the page
+        page_tag = orpml.new_tag('div', attrs={'class': 'page'})
+        page_tag.string = page
+
+        # Append the page tag to the HTML
+        body_tag.append(page_tag)
+
+    logger.info('Finished attaching page to ORPML body')
+
+    beautified_orpml = orpml.prettify(formatter=CustomHTMLFormatter())
+    return str(beautified_orpml)
 
 
 def write_text(s3_client, text, document_uid, destination_bucket=DESTINATION_BUCKET):
@@ -159,8 +212,8 @@ def handler(event, context: LambdaContext):
     )
 
     # Finding the time the object was uploaded
-    date_uploaded = datetime.strptime(event['time'], "%Y-%m-%dT%H:%M:%SZ")
-    date_uploaded_formatted = datetime.strftime(date_uploaded, "%Y-%m-%dT%H:%M:%S")
+    date_uploaded = datetime.strptime(event['time'], '%Y-%m-%dT%H:%M:%SZ')
+    date_uploaded_formatted = datetime.strftime(date_uploaded, '%Y-%m-%dT%H:%M:%S')
 
     # Downloading document and S3 metadata from S3
     s3_client = boto3.client('s3')
@@ -180,14 +233,15 @@ def handler(event, context: LambdaContext):
     assert doc_s3_metadata.get('uuid'), 'Document must have a UUID attached'
 
     # Extract text and metadata from PDF
-    pdf_metadata = extract_pdf_metadata(doc_bytes_io=doc_bytes_io)
-    pdf_metadata['DC.dateSubmitted'] = date_uploaded_formatted
+    pdf_meta_tags = extract_pdf_metadata(doc_bytes_io=doc_bytes_io)
+    pdf_meta_tags.append(
+        {'name': 'DC.dateSubmitted', 'content': date_uploaded_formatted})
     text_pages = extract_pdf_text(doc_bytes_io=doc_bytes_io)
 
     # Build ORPML document (insert header and body)
     orpml_doc = process_orpml(
         pages=text_pages,
-        pdf_metadata=pdf_metadata,
+        pdf_meta_tags=pdf_meta_tags,
         s3_metadata=doc_s3_metadata
     )
 
